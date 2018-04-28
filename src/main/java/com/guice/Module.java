1e001d77c6b075610bb3caffd00aefe5465358ff
blackmorse@live.com <AVKOCH15@mts.ru>
2018-04-28 16:00:12 +0300

    5. Multibindings

--- src/main/java/com/guice/Module.java
+++ src/main/java/com/guice/Module.java
@@ -3,6 +3,7 @@ package com.guice;
 import com.google.inject.AbstractModule;
 import com.google.inject.TypeLiteral;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
+import com.google.inject.multibindings.MapBinder;
 import com.guice.workers.Worker;
 import com.guice.workers.Worker1;
 import com.guice.workers.Worker2;
@@ -15,5 +16,9 @@ public class Module extends AbstractModule{
 
         install(new FactoryModuleBuilder().implement(Worker.class, Worker2.class)
                 .build(new TypeLiteral<WorkerFactory<Worker2>>() {}));
+
+        MapBinder<String, WorkerFactory> binder = MapBinder.newMapBinder(binder(), String.class, WorkerFactory.class);
+        binder.addBinding("case1").to(new TypeLiteral<WorkerFactory<Worker1>>(){});
+        binder.addBinding("case2").to(new TypeLiteral<WorkerFactory<Worker2>>(){});
     }
 }
--- src/main/java/com/guice/RequestHandler.java
+++ src/main/java/com/guice/RequestHandler.java
@@ -3,29 +3,21 @@ package com.guice;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.guice.workers.Worker;
-import com.guice.workers.Worker1;
-import com.guice.workers.Worker2;
+
+import java.util.Map;
 
 @Singleton
 public class RequestHandler {
-    private WorkerFactory<Worker1> worker1Factory;
-    private WorkerFactory<Worker2> worker2Factory;
+    private Map<String, WorkerFactory> workerFactoryMap;
 
     @Inject
-    public RequestHandler(WorkerFactory<Worker1> worker1Factory,
-                          WorkerFactory<Worker2> worker2Factory) {
-        this.worker1Factory = worker1Factory;
-        this.worker2Factory = worker2Factory;
+    public RequestHandler(Map<String, WorkerFactory> workerFactoryMap) {
+        this.workerFactoryMap = workerFactoryMap;
     }
 
     public void handleRequest(Request request) {
-        Worker worker = null;
-        if (request.parameter.equals("case1")) {
-            worker = worker1Factory.createWorker(request.argument);
-        } else if (request.parameter.equals("case2")) {
-            worker = worker2Factory.createWorker(request.argument);
-        }
-
+        Worker worker = workerFactoryMap.get(request.parameter)
+                .createWorker(request.argument);
         worker.doWork();
     }
 }
